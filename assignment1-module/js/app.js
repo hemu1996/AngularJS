55e8bc26ff9df6037fd197e7e8a5160e99887de8
hemant motwani <hemant motwani>
2016-09-29 23:37:21 +0530

    work on CSS

--- assignment1-module/js/app.js
+++ assignment1-module/js/app.js
@@ -6,19 +6,34 @@
    .controller('lunchCheckController', function($scope) {
        $scope.message="";
        $scope.check = function(){
-           console.log($scope.value);
+         //s  console.log($scope.value);
            if($scope.value==undefined || $scope.value=="")
                {
                $scope.message="Please enter data first";
+               $scope.colour="'color':'red'";
+                   console.log($scope.colour);
            }
            else{
            var stringToSplit = $scope.value;
            var arrayOfStrings = stringToSplit.split(',');
-           if(arrayOfStrings.length <=3){
+           var count=0;
+            //   console.log(arrayOfStrings[0]);
+               for(var i=0;i<arrayOfStrings.length;i++){
+                   if(arrayOfStrings[i]==""||arrayOfStrings[i]==" "){
+                       continue;
+                   }
+                   else{
+                       count++;
+                   }
+               }
+           if(count <=3){
            $scope.message="Enjoy!";
-           }
+           $scope.colour="'color':'green'";
+               console.log($scope.colour);           }
            else{
                $scope.message="Too much!";
+               $scope.colour="{'color':'green'}";
+               console.log(colour);
            }
                }
        };
